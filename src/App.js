81a994e130a9152cb59c42fecc0d6ba86eb96fa0
Kwenyerechukwu Paul <paulkwenye@gmail.com>
2023-10-24 15:12:54 +0300

    update

--- src/App.js
+++ src/App.js
@@ -25,28 +25,35 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(1);
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          text={el.text}
+          num={i}
+          key={el.title}
+        />
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [IsOpen, setIsOpen] = useState(false);
-
+function AccordionItem({ num, title, text, curOpen, onOpen }) {
+  const isOpen = num === curOpen;
   function handleToggle() {
-    setIsOpen((IsOpen) => !IsOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
-    <div className={`item ${IsOpen ? "open" : " "}`} onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : " "}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">{IsOpen ? "-" : "+"}</p>
-      {IsOpen && <div className="content-box">{text}</div>}
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
